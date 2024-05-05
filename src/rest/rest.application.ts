2cb7a7f90e8916e2b2b27c292d02016747d9afb5
Pronin Maks <maksimpronin1991@icloud.com>
2024-05-05 16:00:05 +0300

    added express

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -5,14 +5,18 @@ import { Logger } from "../shared/libs/logger/index.js";
 import { Component } from "../shared/types/component.enum.js";
 import { DatabaseClient } from "../shared/libs/database-client/database-client.interface.js";
 import { getMongoURI } from "../shared/helpers/database.js";
+import express, { Express } from 'express';
 
 @injectable()
 export class RestApplication {
+  private server: Express;
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
     @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
-  ) { }
+  ) {
+    this.server = express();
+  }
 
   private async _initDb() {
 
@@ -26,15 +30,21 @@ export class RestApplication {
     return this.databaseClient.connect(mongoUri)
   }
 
+  private async _initServer() {
+    const port = this.config.get('PORT');
+    this.server.listen(port);
+  }
+
   public async init() {
     this.logger.info("RestApplication init");
     this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
 
     this.logger.info('init db');
     await this._initDb();
-
-
     this.logger.info('init db done');
 
+    this.logger.info('Try to init server...');
+    await this._initServer();
+    this.logger.info(`Server started on http://localhost:${this.config.get('PORT')}`);
   }
 }
